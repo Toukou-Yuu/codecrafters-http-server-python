93c7375104e2ab1fd37f29ee07db0b6430d5194d
asuna <1184438613@qq.com>
2024-07-14 23:14:46 +0800

    pass respond with 200

--- app/main.py
+++ app/main.py
@@ -7,9 +7,13 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
+    # 监听本地4221端口
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    # 'b' 表示这是一个字节字符串，而不是普通的Unicode字符串
+    # accept返回两个元组：
+    # 1. 第一个元素是表示与客户端通信的套接字对象（通常称为客户端套接字）。
+    # 2. 第二个元素是客户端的地址信息（例如IP地址和端口号）。
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
