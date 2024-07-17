c531c9a6c50801a00796e6746401ad1fd25e9418
asuna <1184438613@qq.com>
2024-07-17 15:00:37 +0800

    modify client

--- app/server/Server.py
+++ app/server/Server.py
@@ -16,31 +16,31 @@ class Server:
         while True:
             self.SERVER_SOCKET = socket.create_server(("localhost", 4221), reuse_port=True)
             print("listening port: 4221")
-            self.CLIENT_SOCKET, self.CLIENT_ADDR = self.SERVER_SOCKET.accept()
+            client_socket, client_addr = self.SERVER_SOCKET.accept()
             print("accept request, creating threading to handle it......")
-            threading.Thread(target=self.handle_req).start()
+            threading.Thread(target=self.handle_req, args=(client_socket, client_addr)).start()
 
-    def close(self) -> None:
+    def close(self, client_socket) -> None:
         self.SERVER_SOCKET.close()
-        self.CLIENT_SOCKET.close()
+        client_socket.close()
 
-    def handle_req(self) -> None:
-        self.req = Request(self.CLIENT_SOCKET.recv(1024).decode("utf8"))
+    def handle_req(self, client_socket, client_addr) -> None:
+        self.req = Request(client_socket.recv(1024).decode("utf8"))
         print(f"handling request: {self.req.path}")
         if self.req.full_match_endpoint("/"):
-            self.handle_root()
+            self.handle_root(client_socket)
         elif self.req.match_endpoint("/echo"):
             # 读取echo_str：/echo/{echo_str}
-            self.handle_echo()
+            self.handle_echo(client_socket)
         elif self.req.full_match_endpoint("/user-agent"):
             # 读取用户代理
-            self.handle_user_agent()
+            self.handle_user_agent(client_socket)
         else:
-            self.handle_404()
+            self.handle_404(client_socket)
         # self.close()
 
 
-    def handle_root(self) -> None:
+    def handle_root(self, client_socket) -> None:
         print("-----------handle root request-----------")
         resp = Response()
         resp.add_status(HTTP_200)
@@ -48,9 +48,9 @@ class Server:
         resp.add_header("Content-Length: 0")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        self.CLIENT_SOCKET.sendall(constructed)
+        client_socket.sendall(constructed)
 
-    def handle_echo(self) -> None:
+    def handle_echo(self, client_socket) -> None:
         # 读取echo字符串：/echo/{echo_str}
         print("-----------handle echo request-----------")
         req_target = self.req.read_req_target()
@@ -62,9 +62,9 @@ class Server:
         resp.add_header(f"Content-Length: {len(body)}")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        self.CLIENT_SOCKET.sendall(resp.construct_utf8())
+        client_socket.sendall(resp.construct_utf8())
 
-    def handle_user_agent(self) -> None:
+    def handle_user_agent(self, client_socket) -> None:
         # 读取用户代理
         print("-----------handle user-agent-----------")
         resp = Response()
@@ -75,13 +75,13 @@ class Server:
         resp.add_header(f"Content-Length: {len(body)}") 
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        self.CLIENT_SOCKET.sendall(resp.construct_utf8())
+        client_socket.sendall(resp.construct_utf8())
 
-    def handle_404(self) -> None:
+    def handle_404(self, client_socket) -> None:
         print("-----------handle 404-----------")
         resp = Response()
         resp.add_status(HTTP_404)
         resp.add_header("Content-Length: 0")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        self.CLIENT_SOCKET.sendall(resp.construct_utf8())
+        client_socket.sendall(resp.construct_utf8())
