fc927bc60950b2507c66d3c103f57920bef03c2d
asuna <1184438613@qq.com>
2024-07-19 14:04:15 +0800

    add gzip compression header for echo

--- app/server/Server.py
+++ app/server/Server.py
@@ -1,18 +1,23 @@
 import socket
+from socket import socket as type_socket
 import threading
 import sys
-from app.server.Request import Request
-from app.server.Response import Response
+from app.server.request import Request
+from app.server.response import Response
 
 # RFC9112标准: status-line = HTTP-version SP status-code SP [ reason-phrase ]
+# status
 HTTP_200 = "HTTP/1.1 200 OK"
 HTTP_404 = "HTTP/1.1 404 Not Found"
 HTTP_201 = "HTTP/1.1 201 Created"
+# headers
 TEXT_PLAIN = "Content-Type: text/plain"
 FILE_STREAM = "Content-Type: application/octet-stream"
+GZIP = "Content-Encoding: gzip"
+# others
 EMPTY_STR = ""
 UTF8 = "utf-8"
-VALID_ENDPOINT = ["/", "/echo", "/user-agent"]
+VALID_ENDPOINT = ["/", "/echo", "/user-agent", "/files"]
 
 class Aincrad_Server:
     def link_start(self) -> None:
@@ -21,30 +26,30 @@ class Aincrad_Server:
             print("listening port: 4221")
             client_socket, client_addr = self.SERVER_SOCKET.accept()
             print("accept request, creating threading to handle it......")
-            threading.Thread(target=self.handle_req, args=(client_socket, client_addr)).start()
+            threading.Thread(target=self._handle_req, args=(client_socket, client_addr)).start()
 
-    def close(self, client_socket) -> None:
+    def close(self, client_socket: type_socket) -> None:
         self.SERVER_SOCKET.close()
         client_socket.close()
 
-    def handle_req(self, client_socket, client_addr) -> None:
+    def _handle_req(self, client_socket: type_socket, client_addr) -> None:
         self.req = Request(client_socket.recv(1024).decode("utf8"))
-        print(f"handling request: {self.req.path}")
+        print(f"handling request: {self.req.read_req_target()}")
         if self.req.full_match_endpoint("/"):
-            self.handle_root(client_socket)
+            resp = self._handle_root()
         elif self.req.match_endpoint("/echo"):
             # 读取echo_str：/echo/{echo_str}
-            self.handle_echo(client_socket)
+            resp = self._handle_echo()
         elif self.req.full_match_endpoint("/user-agent"):
             # 读取用户代理
-            self.handle_user_agent(client_socket)
+            resp = self._handle_user_agent()
         elif self.req.match_endpoint("/files"):
-            self.handle_file(client_socket)
+            resp = self._handle_file()
         else:
-            self.handle_404(client_socket)
-        # self.close()
+            resp = self._handle_404()
+        client_socket.sendall(resp)
 
-    def handle_root(self, client_socket) -> None:
+    def _handle_root(self) -> bytes:
         print("-----------handle root request-----------")
         resp = Response()
         resp.add_status(HTTP_200)
@@ -52,23 +57,26 @@ class Aincrad_Server:
         resp.add_header("Content-Length: 0")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        client_socket.sendall(constructed)
+        return constructed
 
-    def handle_echo(self, client_socket) -> None:
+    def _handle_echo(self) -> bytes:
         # 读取echo字符串：/echo/{echo_str}
         print("-----------handle echo request-----------")
+        resp = Response()
+        encoding = self.req.read_header("Content-Encoding")
+        if encoding == "gzip":
+            resp.add_header(GZIP)
         req_target = self.req.read_req_target()
         body = req_target[len("/echo/"):]
-        resp = Response()
         resp.add_status(HTTP_200)
         resp.add_body(body)
         resp.add_header(TEXT_PLAIN)
         resp.add_header(f"Content-Length: {len(body)}")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        client_socket.sendall(resp.construct_utf8())
+        return constructed
 
-    def handle_user_agent(self, client_socket) -> None:
+    def _handle_user_agent(self) -> bytes:
         # 读取用户代理
         print("-----------handle user-agent-----------")
         resp = Response()
@@ -79,9 +87,9 @@ class Aincrad_Server:
         resp.add_header(f"Content-Length: {len(body)}") 
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        client_socket.sendall(resp.construct_utf8())
+        return constructed
 
-    def handle_file(self, client_socket) -> None:
+    def _handle_file(self) -> bytes:
         # ./your_program.sh --directory /tmp/
         # path = /files/{filename}
         directory = sys.argv[2]
@@ -104,13 +112,13 @@ class Aincrad_Server:
                 resp.add_body(body)
         except Exception as e:
             resp.add_status(HTTP_404)
-        client_socket.sendall(resp.construct_utf8())
+        return resp.construct_utf8()
 
-    def handle_404(self, client_socket) -> None:
+    def _handle_404(self) -> bytes:
         print("-----------handle 404-----------")
         resp = Response()
         resp.add_status(HTTP_404)
         resp.add_header("Content-Length: 0")
         constructed = resp.construct_utf8()
         print(f"response: {constructed}")
-        client_socket.sendall(resp.construct_utf8())
+        return constructed
